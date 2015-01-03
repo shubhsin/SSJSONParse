0718bc793f05008dbe246c9e532b21c590a5b50f
shubhsin <luk4shubhs@gmail.com>
2015-01-03 18:53:00 +0530

    Most Basic functionality done

---
+++ README.md
@@ -0,0 +1,29 @@
+##SSJSONParse
+
+Its the simplest library for parsing JSON Array or Dictionary.
+Mostly meant for begineer iOS Developers.
+
+##Usage
+
+1. Add the file SSJSONModel.h and SSJSONModel.m in your Xcode project.
+2.`#import SSJSONModel.h` in your ViewController file where you need the JSON Data.
+3. Make an instance of SSJSONModel
+```objective-c
+    SSJSONModel * jsonInstance1;
+jsonInstance1 =[[SSJSONModel alloc] initWithDelegate:self];
+[jsonInstance1 sendRequestWithUrl:@"your url"];
+```
+4. Implement this method
+```objective-c
+- (void)jsonRequestDidCompleteWithDict:(NSDictionary *)dict model:(SSJSONModel *)JSONModel
+{
+if(JSONModel == jsonInstance1)
+{
+NSLog(@"dict is %@",jsonInstance1.parsedJsonData);
+}
+}
+
+*Your jsonInstance1.parsedJsonData is the data you need. Assign it to a NSDictionary or NSArray instance according to whatever your data type is.
+
+
+
--- SSJSONParse/SSViewController.m
+++ SSJSONParse/SSViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "SSViewController.h"
+#import "SSJSONModel.h"
 
-@interface SSViewController ()
+@interface SSViewController () <SSJSONModelDelegate>
+{
+    NSArray * jsonData;
+    SSJSONModel * jsonInstance1;
+    SSJSONModel * jsonInstance2;
+}
 
 @end
 
@@ -18,7 +24,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor redColor];
+    NSURL *mainUrl = [NSURL URLWithString:@"http://shubhapple.site50.net/Event.plist"];
+     NSURL *mainUrl2 = [NSURL URLWithString:@"http://shubhapple.site50.net/Event.plist"];
+    jsonInstance1 =[[SSJSONModel alloc] initWithDelegate:self];
+    [jsonInstance1 sendRequestWithUrl:mainUrl];
+    jsonInstance2 = [[SSJSONModel alloc]initWithDelegate:self];
+    [jsonInstance2 sendRequestWithUrl:mainUrl2];
+    
 }
+
+- (void)jsonRequestDidCompleteWithDict:(NSDictionary *)dict model:(SSJSONModel *)JSONModel
+{
+    if(JSONModel == jsonInstance1)
+    {
+        NSLog(@"dict is %@",jsonInstance1.parsedJsonData);
+    }
+    else if (JSONModel == jsonInstance2)
+    {
+        NSLog(@"dict 2 is %@",jsonInstance2.parsedJsonData);
+
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
